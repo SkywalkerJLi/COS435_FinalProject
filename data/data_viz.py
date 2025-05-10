337f9f72f256ccaeb6c7b184f8a25cd4b5131741
GForceSquared <galaxyforce2000@gmail.com>
2025-05-10 00:30:22 -0400

    data + plots

--- data/data_viz.py
+++ data/data_viz.py
@@ -8,51 +8,67 @@ df_pickplace_drawer = pd.read_csv("progress_pickplace_drawer.csv")
 df_pickplace_table = pd.read_csv("progress_pickplacetable.csv")
 df_drawer = pd.read_csv("progress_drawer.csv")
 df_drawer_nolayer = pd.read_csv("progress_drawer_nolayer.csv")
+df_drawer_unstable = pd.read_csv("progress_drawer_unstable.csv")
+df_pickplace_drawer_entropy = pd.read_csv("progress_pickplace_drawer_entropy0.1.csv")
+
 
 pushblock_drawer_mean = df_pushblock_drawer['eval/state_desired_goal/final/overall_success Mean'] 
 pickplace_drawer_mean = df_pickplace_drawer['eval/state_desired_goal/final/overall_success Mean'] 
 pickplace_table_mean = df_pickplace_table['eval/state_desired_goal/final/overall_success Mean'] 
 drawer_mean = df_drawer['eval/state_desired_goal/final/overall_success Mean'] 
 drawer_nolayer_mean = df_drawer_nolayer['eval/state_desired_goal/final/overall_success Mean'] 
+pickplace_drawer_entropy_mean = df_pickplace_drawer_entropy['eval/state_desired_goal/final/overall_success Mean']
 
+drawer_actor_loss = df_drawer['trainer/eval/Policy Loss'] 
+drawer_unstable_actor_loss = df_drawer_unstable['trainer/eval/Policy Loss'] 
 
-pushblock_drawer_mean_overall = df_pushblock_drawer['expl/Returns Mean'] 
-pickplace_drawer_mean_overall = df_pickplace_drawer['eval/state_desired_goal/overall_success Mean'] 
-pickplace_table_mean_overall = df_pickplace_table['expl/Returns Mean'] 
-drawer_mean_overall = df_drawer['eval/state_desired_goal/overall_success Mean'] 
-
-pushblock_drawer_std = df_pushblock_drawer['eval/state_desired_goal/final/overall_success Std'] 
-pickplace_drawer_std = df_pickplace_drawer['eval/state_desired_goal/final/overall_success Std'] 
-pickplace_table_std = df_pickplace_table['eval/state_desired_goal/final/overall_success Std'] 
-drawer_std = df_drawer['eval/state_desired_goal/final/overall_success Std'] 
-drawer_nolayer_std = df_drawer_nolayer['eval/state_desired_goal/final/overall_success Std'] 
-
-pushblock_drawer_std_overall = df_pushblock_drawer['expl/Returns Std'] 
-pickplace_drawer_std_overall = df_pickplace_drawer['eval/state_desired_goal/final/overall_success Std'] 
-pickplace_table_std_overall = df_pickplace_table['expl/Returns Std'] 
-drawer_std_overall = df_drawer['eval/state_desired_goal/final/overall_success Std'] 
-
-drawer_nolayer_mean_smoothed = gaussian_filter1d(drawer_nolayer_mean, 3)
-drawer_nolayer_std_smoothed = gaussian_filter1d(drawer_nolayer_std, 3)
-
-grad_steps_per_epoch = 2000
-new_indices = np.linspace(0, len(drawer_nolayer_mean_smoothed) - 1, len(drawer_nolayer_mean_smoothed) * grad_steps_per_epoch)
-drawer_nolayer_mean_smoothed_extended = np.interp(new_indices, np.arange(len(drawer_nolayer_mean_smoothed)), drawer_nolayer_mean_smoothed)
-drawer_nolayer_std_smoothed_extended = np.interp(new_indices, np.arange(len(drawer_nolayer_std_smoothed)), drawer_nolayer_std_smoothed)
-
-x = np.linspace(0, len(drawer_nolayer_mean_smoothed) * grad_steps_per_epoch, len(drawer_nolayer_mean_smoothed) * grad_steps_per_epoch)
 
 plt.figure(figsize=(6, 4))
-plt.plot(x, drawer_nolayer_mean_smoothed_extended, label = 'push block, open drawer')
-plt.fill_between(x,
-                 drawer_nolayer_mean_smoothed_extended - drawer_nolayer_std_smoothed_extended,
-                 drawer_nolayer_mean_smoothed_extended + drawer_nolayer_std_smoothed_extended,
-                 alpha=0.2)
-plt.xlabel('Gradient Steps')
-plt.ylabel('Evaluation Success Rate')
-plt.title('Drawer (No Layer)')
-plt.tight_layout()
+plt.plot(pickplace_drawer_entropy_mean, label = "Stable Contrastive RL")
+plt.title("Policy Loss over Epochs for Drawer Environment")
+plt.xlabel("Epochs")
+plt.xlim(0,230)
+plt.ylabel("Policy Loss")
+plt.legend()
 plt.show()
+
+# pushblock_drawer_mean_overall = df_pushblock_drawer['expl/Returns Mean'] 
+# pickplace_drawer_mean_overall = df_pickplace_drawer['eval/state_desired_goal/overall_success Mean'] 
+# pickplace_table_mean_overall = df_pickplace_table['expl/Returns Mean'] 
+# drawer_mean_overall = df_drawer['eval/state_desired_goal/overall_success Mean'] 
+
+# pushblock_drawer_std = df_pushblock_drawer['eval/state_desired_goal/final/overall_success Std'] 
+# pickplace_drawer_std = df_pickplace_drawer['eval/state_desired_goal/final/overall_success Std'] 
+# pickplace_table_std = df_pickplace_table['eval/state_desired_goal/final/overall_success Std'] 
+# drawer_std = df_drawer['eval/state_desired_goal/final/overall_success Std'] 
+# drawer_nolayer_std = df_drawer_nolayer['eval/state_desired_goal/final/overall_success Std'] 
+
+# pushblock_drawer_std_overall = df_pushblock_drawer['expl/Returns Std'] 
+# pickplace_drawer_std_overall = df_pickplace_drawer['eval/state_desired_goal/final/overall_success Std'] 
+# pickplace_table_std_overall = df_pickplace_table['expl/Returns Std'] 
+# drawer_std_overall = df_drawer['eval/state_desired_goal/final/overall_success Std'] 
+
+# drawer_nolayer_mean_smoothed = gaussian_filter1d(drawer_nolayer_mean, 3)
+# drawer_nolayer_std_smoothed = gaussian_filter1d(drawer_nolayer_std, 3)
+
+# grad_steps_per_epoch = 2000
+# new_indices = np.linspace(0, len(drawer_nolayer_mean_smoothed) - 1, len(drawer_nolayer_mean_smoothed) * grad_steps_per_epoch)
+# drawer_nolayer_mean_smoothed_extended = np.interp(new_indices, np.arange(len(drawer_nolayer_mean_smoothed)), drawer_nolayer_mean_smoothed)
+# drawer_nolayer_std_smoothed_extended = np.interp(new_indices, np.arange(len(drawer_nolayer_std_smoothed)), drawer_nolayer_std_smoothed)
+
+# x = np.linspace(0, len(drawer_nolayer_mean_smoothed) * grad_steps_per_epoch, len(drawer_nolayer_mean_smoothed) * grad_steps_per_epoch)
+
+# plt.figure(figsize=(6, 4))
+# plt.plot(x, drawer_nolayer_mean_smoothed_extended, label = 'push block, open drawer')
+# plt.fill_between(x,
+#                  drawer_nolayer_mean_smoothed_extended - drawer_nolayer_std_smoothed_extended,
+#                  drawer_nolayer_mean_smoothed_extended + drawer_nolayer_std_smoothed_extended,
+#                  alpha=0.2)
+# plt.xlabel('Gradient Steps')
+# plt.ylabel('Evaluation Success Rate')
+# plt.title('Drawer (No Layer)')
+# plt.tight_layout()
+# plt.show()
 
 # x = np.linspace(0, len(pickplace_drawer_mean), len(pickplace_drawer_mean))
 # plt.figure(figsize=(6, 4))
