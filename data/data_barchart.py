2da714eeae8acefa82497e4a8f42f5c30fd31b16
GForceSquared <galaxyforce2000@gmail.com>
2025-05-11 16:26:37 -0400

    clean up

--- data/data_barchart.py
+++ data/data_barchart.py
@@ -12,6 +12,7 @@ df_drawer_noaug = pd.read_csv("progress_noaug_2.csv")
 df_drawer_noinit = pd.read_csv("progress_drawer_noinit.csv")
 df_drawer_512_2 = pd.read_csv("progress_drawer_512_2.csv")
 df_drawer_1024_2 = pd.read_csv("progress_1024_2.csv")
+df_drawer_512_4 = pd.read_csv("progress_512_4.csv")
 
 df_drawer_unstable = pd.read_csv("progress_drawer_unstable.csv")
 df_pickplace_drawer_unstable = pd.read_csv("progress_pickplace_drawer_unstable.csv")
@@ -28,6 +29,7 @@ drawer_noaug_mean = df_drawer_noaug['eval/state_desired_goal/final/overall_succe
 df_drawer_noinit_mean = df_drawer_noinit['eval/state_desired_goal/final/overall_success Mean'] 
 df_drawer_512_2_mean = df_drawer_512_2['eval/state_desired_goal/final/overall_success Mean'] 
 df_drawer_1024_2_mean = df_drawer_1024_2['eval/state_desired_goal/final/overall_success Mean'] 
+df_drawer_512_4_mean = df_drawer_512_4['eval/state_desired_goal/final/overall_success Mean'] 
 
 drawer_unstable_mean = df_drawer_unstable['eval/state_desired_goal/final/overall_success Mean'] 
 pickplace_drawer_unstable_mean = df_pickplace_drawer_unstable['eval/state_desired_goal/final/overall_success Mean'] 
@@ -36,13 +38,13 @@ pushblock_drawer_unstable_mean = df_pushblock_drawer_unstable['eval/state_desire
 
 
 
-# print(np.mean(drawer_mean))
-# print(np.mean(drawer_nolayer_mean))
-# print(np.mean(drawer_noaug_mean))
-# print(np.mean(df_drawer_noinit_mean))
-# print(drawer_mean.iloc[50])
-# print(drawer_nolayer_mean.iloc[45])
-# print(drawer_noaug_mean.iloc[45])
+# print(drawer_mean)
+# print(drawer_nolayer_mean)
+# print(drawer_noaug_mean)
+# print(df_drawer_noinit_mean)
+# print(drawer_mean)
+# print(drawer_nolayer_mean)
+# print(drawer_noaug_mean)
 
 pushblock_drawer_std = df_pushblock_drawer['eval/state_desired_goal/final/overall_success Std'] 
 pickplace_drawer_std = df_pickplace_drawer['eval/state_desired_goal/final/overall_success Std'] 
@@ -53,67 +55,69 @@ drawer_noaug_std = df_drawer_noaug['eval/state_desired_goal/final/overall_succes
 drawer_noinit_std = df_drawer_noinit['eval/state_desired_goal/final/overall_success Std'] 
 df_drawer_512_2_std = df_drawer_512_2['eval/state_desired_goal/final/overall_success Std'] 
 df_drawer_1024_2_std = df_drawer_1024_2['eval/state_desired_goal/final/overall_success Std'] 
+df_drawer_512_4_std = df_drawer_512_4['eval/state_desired_goal/final/overall_success Std'] 
+
 
 drawer_unstable_std = df_drawer_unstable['eval/state_desired_goal/final/overall_success Std'] 
 pickplace_drawer_unstable_std = df_pickplace_drawer_unstable['eval/state_desired_goal/final/overall_success Std'] 
 pickplace_table_unstable_std = df_pickplace_table_unstable['eval/state_desired_goal/final/overall_success Std'] 
 pushblock_drawer_unstable_std = df_pushblock_drawer_unstable['eval/state_desired_goal/final/overall_success Std'] 
 
-labels = ['drawer', 'pick & place \n (table)', 'pick & place \n (drawer)', 'push block \n open drawer']
-x = np.arange(len(labels))  # label locations
-width = 0.35  # width of the bars
+# labels = ['drawer', 'pick & place \n (table)', 'pick & place \n (drawer)', 'push block \n open drawer']
+# x = np.arange(len(labels))  # label locations
+# width = 0.35  # width of the bars
 
-stable_means = [np.mean(drawer_mean), np.mean(pickplace_table_mean), np.mean(pickplace_drawer_mean), np.mean(pushblock_drawer_mean)]
-unstable_means = [np.mean(drawer_unstable_mean), np.mean(pickplace_table_unstable_mean), np.mean(pickplace_drawer_unstable_mean), np.mean(pushblock_drawer_unstable_mean)]
+# stable_means = [drawer_mean, pickplace_table_mean, pickplace_drawer_mean, pushblock_drawer_mean]
+# unstable_means = [drawer_unstable_mean, pickplace_table_unstable_mean, pickplace_drawer_unstable_mean, pushblock_drawer_unstable_mean]
 
-stable_stds = [np.mean(drawer_std), np.mean(pickplace_table_std), np.mean(pickplace_drawer_std), np.mean(pushblock_drawer_std)]
-unstable_stds = [np.mean(drawer_unstable_std), np.mean(pickplace_table_unstable_std), np.mean(pickplace_drawer_unstable_std), np.mean(pushblock_drawer_unstable_std)]
-
-
-fig, ax = plt.subplots()
-bars1 = ax.bar(x - width/2, stable_means, width, yerr=stable_stds, label='Stable Contrastive RL', capsize=5)
-bars2 = ax.bar(x + width/2, unstable_means, width, yerr=unstable_stds, label='Contrastive RL', capsize=5)
-
-# Labels and layout
-ax.set_ylabel('Success Rate')
-ax.set_xticks(x)
-ax.set_xticklabels(labels)
-ax.legend()
-plt.tight_layout()
-
-plt.show()
+# stable_stds = [drawer_std, pickplace_table_std, pickplace_drawer_std, pushblock_drawer_std]
+# unstable_stds = [drawer_unstable_std, pickplace_table_unstable_std, pickplace_drawer_unstable_std, pushblock_drawer_unstable_std]
 
 
-# categories = ["3-layer CNN + (1024, 4) MLP", "3-layer CNN + (1024, 2) MLP", "3-layer CNN + (512, 2) MLP"]
-# values = [np.mean(drawer_mean), np.mean(df_drawer_1024_2_mean), np.mean(df_drawer_512_2_mean)]
-# stds = [np.mean(drawer_std), np.mean(df_drawer_1024_2_std), np.mean(df_drawer_512_2_std)]
-# colors = ['#1f77b4', '#a6c8e0', '#a6c8e0']
+# fig, ax = plt.subplots()
+# bars1 = ax.bar(x - width/2, stable_means, width, yerr=stable_stds, label='Stable Contrastive RL', capsize=5)
+# bars2 = ax.bar(x + width/2, unstable_means, width, yerr=unstable_stds, label='Contrastive RL', capsize=5)
+
+# # Labels and layout
+# ax.set_ylabel('Success Rate')
+# ax.set_xticks(x)
+# ax.set_xticklabels(labels)
+# ax.legend()
+# plt.tight_layout()
+
+# plt.show()
 
 
-# print(np.mean(drawer_std))
-# print(np.mean(drawer_nolayer_std))
-# print(np.mean(drawer_noaug_std))
-# print(np.mean(drawer_noinit_std))
-# print(drawer_std.iloc[50])
-# print(drawer_nolayer_std.iloc[45])
-# print(drawer_noaug_std.iloc[45])
+categories = ["3-layer CNN + (1024, 4) MLP", "3-layer CNN + (1024, 2) MLP", "3-layer CNN + (512, 4) MLP", "3-layer CNN + (512, 2) MLP"]
+values = [drawer_mean, df_drawer_1024_2_mean, df_drawer_512_4_mean, df_drawer_512_2_mean]
+stds = [drawer_std, df_drawer_1024_2_std, df_drawer_512_4_std, df_drawer_512_2_std]
+colors = ['#1f77b4', '#a6c8e0', '#a6c8e0', '#a6c8e0']
+
+
+# print(drawer_std)
+# print(drawer_nolayer_std)
+# print(drawer_noaug_std)
+# print(drawer_noinit_std)
+# print(drawer_std)
+# print(drawer_nolayer_std)
+# print(drawer_noaug_std)
 
 
 # categories = ["Stable Contrastive RL", "w/o cold initialization", "w/o layer normalization", "w/o data augmentation"]
-# values = [np.mean(drawer_mean), np.mean(df_drawer_noinit_mean), np.mean(drawer_nolayer_mean.iloc), np.mean(drawer_noaug_mean.iloc)]
-# stds = [np.mean(drawer_std), np.mean(drawer_noinit_std), np.mean(drawer_nolayer_std.iloc), np.mean(drawer_noaug_std.iloc)]
+# values = [drawer_mean, df_drawer_noinit_mean, drawer_nolayer_mean.iloc, drawer_noaug_mean.iloc]
+# stds = [drawer_std, drawer_noinit_std, drawer_nolayer_std.iloc, drawer_noaug_std.iloc]
 
 # categories = ["3-layer CNN + (1024, 4) MLP", "3-layer CNN + (1024, 2) MLP", "3-layer CNN + (512, 2) MLP"]
-# values = [np.mean(drawer_mean), np.mean(df_drawer_1024_2_mean), np.mean(df_drawer_512_2_mean)]
-# stds = [np.mean(drawer_std), np.mean(df_drawer_1024_2_std), np.mean(df_drawer_512_2_std)]
+# values = [drawer_mean, df_drawer_1024_2_mean, df_drawer_512_2_mean]
+# stds = [drawer_std, df_drawer_1024_2_std, df_drawer_512_2_std]
 # colors = ['#1f77b4', '#a6c8e0', '#a6c8e0']
 
 
-# plt.figure(figsize=(12,6))
-# plt.bar(categories, values,  yerr = stds, color = colors, capsize = 5)
+plt.figure(figsize=(12,6))
+plt.bar(categories, values,  yerr = stds, color = colors, capsize = 5)
 
-# plt.ylabel('Success Rate')
-# plt.title('Network Architecture')
+plt.ylabel('Success Rate')
+plt.title('Network Architecture')
 
-# # Show plot
-# plt.show()
+# Show plot
+plt.show()
